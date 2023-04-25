2c3e8ba14e74730312130fbb13986c81608e4d88
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-04-25 12:45:19 -0500

    Edited style and functionality

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Get the current date and display it at the top of the page
+// Current date and display it at the top of the page
 var currentDate = dayjs().format("dddd, MMMM D");
 $("#currentDay").text(currentDate);
 
-// Get the current hour in 24-hour format
-var currentHour = dayjs().format("H");
 
 // Function to color code time blocks based on current time
 function updateSchedule() {
+  var currentHour = dayjs().format("H");
   $(".time-block").each(function() {
     var blockHour = parseInt($(this).attr("id").split("-")[1]);
     
@@ -26,7 +25,7 @@ function updateSchedule() {
   });
 }
 
-// Call updateSchedule function to color code time blocks on page load
+// Call updateSchedule function to color code time blocks when page loads.
 updateSchedule();
 
 // Call updateSchedule function every 5 minutes to keep time blocks color coded
@@ -44,12 +43,12 @@ function saveEvent() {
 
   var messageElement = $("<p>")
     .addClass("save-message")
-    .text("Event saved!")
+    .text("Saved!")
     .hide();
   timeBlock.append(messageElement);
 
-  // Make message element fade out after 2 seconds
-  messageElement.fadeIn(500).delay(1500).fadeOut(500, function() {
+  // Fade Saved! after 2 seconds
+  messageElement.fadeIn(500).delay(500).fadeOut(500, function() {
     $(this).remove();
   });
 }
@@ -69,5 +68,3 @@ function loadEvents() {
 // Call loadEvents function to load saved events on page load
 loadEvents();
 
-// Add event listener to save button
-$(".saveBtn").on("click", saveEvent);
